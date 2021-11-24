d2a04b2fadb962eb57c7378c38534e6c49c0fe7d
Tseng Shih Yang <iamdog43gmail.com>
2021-11-24 16:22:59 +0800

    bootstrap 應用

---
+++ y1124/img.php
@@ -0,0 +1,14 @@
+<?php
+
+$data = [
+["id"=>"1","src"=>"01","chinese" => "long time ago...."],
+["id"=>"2","src"=>"02","chinese" => "the story going on2"],
+["id"=>"3","src"=>"02","chinese" => "the story going on3"],
+["id"=>"4","src"=>"02","chinese" => "the story going on4"],
+
+];
+
+print_r($data[0]['id']);
+
+
+?>
